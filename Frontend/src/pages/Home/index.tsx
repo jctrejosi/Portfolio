c2019140bcf5d017a325ef745637f856a79ff4bd
Juan Carlos Trejos Iglesias <juan.trejos@arandasoft.com>
2025-08-06 00:38:14 -0500

    Agregar componente ScrollSnap y estilos, implementar desplazamiento suave entre secciones

--- Frontend/src/pages/Home/index.tsx
+++ Frontend/src/pages/Home/index.tsx
@@ -1,3 +1,24 @@
+import { ScrollSnap } from "@/components/ScrollSnap";
+
+const example1 = (
+  <div>
+    <h1>Section 1</h1>
+    <p>This is the first section.</p>
+  </div>
+);
+const example2 = (
+  <div>
+    <h1>Section 2</h1>
+    <p>This is the second section.</p>
+  </div>
+);
+const example3 = (
+  <div>
+    <h1>Section 3</h1>
+    <p>This is the third section.</p>
+  </div>
+);
+
 export const Home = () => {
-  return <div>Home Page</div>;
+  return <ScrollSnap>{[example1, example2, example3]}</ScrollSnap>;
 };
